fdc2328f34d3c6959d7cda3052daa296ae7f4479
Justin <justinzw2xep@gmail.com>
2022-08-20 17:46:24 +0200

    removed unused files

---
+++ {{ cookiecutter.repo_name }}/notebooks/local_package_helper.py
@@ -0,0 +1,17 @@
+## this code is need in a jupyter notebook to actively work on or use the package
+
+# import sys
+# import os
+
+# PROJ_ROOT = os.path.join(os.pardir)
+
+# %load_ext autoreload
+# %autoreload 1
+
+# # add the 'src' directory as one where we can import modules
+# src_dir = os.path.join(PROJ_ROOT, "local_package")
+# sys.path.append(src_dir)
+
+# # import my method from the source code
+# %aimport local_package.file_name
+# from local_package.file_name import function_name
